32a4664466e68f8681f6e886591a6349124bb2b8
VladOlevskiy <99341001+VladOlevskiy@users.noreply.github.com>
2024-03-16 10:03:38 +0200

    delate icons

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,9 @@ import "./App.css";
 import { Wrapper } from "./App-styled";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Wrapper>
       <h1>Beauty Estetic Company</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </Wrapper>
   );
 }
